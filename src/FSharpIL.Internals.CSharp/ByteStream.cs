e15dd16fa70822517662f278db6ee989a3b83338
davnavr <davidcalieln@gmail.com>
2020-08-23 19:17:14 -0700

    Moved some code to C# for focus on mutability

---
+++ src/FSharpIL.Internals.CSharp/ByteStream.cs
@@ -0,0 +1,83 @@
+﻿namespace FSharpIL
+{
+    using System;
+    using System.IO;
+
+    internal class ByteStream : IDisposable
+    {
+        private readonly Stream stream;
+        private bool disposed = false;
+
+        internal ByteStream(string name, Stream stream)
+        {
+            this.stream = stream;
+            this.Name = name;
+        }
+
+        internal string Name { get; }
+
+        internal uint Position { get; private set; }
+
+        internal byte? ReadByte() =>
+            this.stream.ReadByte() switch
+            {
+                -1 => null,
+                int value => (byte)value
+            };
+
+        internal byte[]? ReadBytes(int count)
+        {
+            var results = new byte[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                switch (this.ReadByte())
+                {
+                    case null:
+                        return null;
+                    case var value:
+                        results[i] = value.Value;
+                        continue;
+                }
+            }
+
+            return results;
+        }
+
+        /// <summary>
+        /// Attempts to skip to the specified <paramref name="offset"/>.
+        /// </summary>
+        /// <param name="offset">The offset from the beginning of the stream to skip to.</param>
+        /// <returns><see langword="null"/> if successful, otherwise the current position or <c>0</c> if the offset is less than the current position.</returns>
+        internal uint? TryMove(uint offset)
+        {
+            if (offset < this.Position)
+            {
+                return 0;
+            }
+
+            while (this.Position < offset)
+            {
+                switch (this.ReadByte())
+                {
+                    case null:
+                        return this.Position;
+                    default:
+                        this.Position += 1;
+                        continue;
+                }
+            }
+
+            return null;
+        }
+
+        public void Dispose()
+        {
+            if (!this.disposed)
+            {
+                this.disposed = true;
+                this.stream.Close();
+            }
+        }
+    }
+}
---
+++ src/FSharpIL.Internals.CSharp/PEReader.cs
@@ -0,0 +1,30 @@
+﻿namespace FSharpIL
+{
+    using System.IO;
+    using Microsoft.FSharp.Core;
+    using ReadResult = Microsoft.FSharp.Core.FSharpResult<Types.PortableExecutable, Types.ReadError>;
+    using static Types;
+
+    internal sealed class PEReader : FSharpFunc<Stream, ReadResult>
+    {
+        private readonly string name;
+
+        internal PEReader(string name)
+        {
+            this.name = name;
+        }
+
+        public override ReadResult Invoke(Stream stream)
+        {
+            using var source = new ByteStream(this.name, stream);
+            var result = new PortableExecutable(DosStub.NewDosStub(0));
+            ReadError? error = null;
+
+            return error switch
+            {
+                null => ReadResult.NewOk(result),
+                ReadError err => ReadResult.NewError(err),
+            };
+        }
+    }
+}
