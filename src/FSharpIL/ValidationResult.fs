ee8858ae16a1ff6b12534a58b34a7e057e0f9a6f
davnavr <davidcalieln@gmail.com>
2021-01-03 14:08:32 -0800

    Add type to represent warnings and errors during creation of metadata

---
+++ src/FSharpIL/ValidationResult.fs
@@ -0,0 +1,27 @@
+﻿namespace FSharpIL
+
+/// <summary>
+/// Represents a violation of a Common Language Specification rule (I.7).
+/// </summary>
+type ClsCheck =
+    | PointerTypeUsage
+
+/// II.22.1
+type ValidationResult<'Result, 'Warning, 'Error> =
+    | ValidationSuccess of 'Result * ClsCheck list
+    | ValidationWarning of 'Result * ClsCheck list * 'Warning
+    | ValidationError of 'Error
+
+[<RequireQualifiedAccess>]
+module ValidationResult =
+    let toOption value =
+        match value with
+        | ValidationSuccess (result, _)
+        | ValidationWarning (result, _, _) -> Some result
+        | ValidationError _ -> None
+
+    let toResult value =
+        match value with
+        | ValidationSuccess (result, _)
+        | ValidationWarning (result, _, _) -> Ok result
+        | ValidationError err -> Error err
