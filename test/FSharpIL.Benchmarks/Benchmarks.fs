8e96b487a794fe1299ebf8ea294bb96fcd691e36
davnavr <davidcalieln@gmail.com>
2021-02-09 11:09:22 -0800

    Add benchmark to compare with Mono.Cecil

--- test/FSharpIL.Benchmarks/Benchmarks.fs
+++ test/FSharpIL.Benchmarks/Benchmarks.fs
@@ -5,17 +5,19 @@ open BenchmarkDotNet.Running
 
 open System
 open System.Collections.Immutable
+open System.IO
+
+open Mono.Cecil
+open Mono.Cecil.Cil
 
 open FSharpIL.Metadata
 open FSharpIL.PortableExecutable
 
 [<StatisticalTestColumn>]
 [<MemoryDiagnoser>]
-[<SimpleJob(targetCount = 30)>]
 type HelloWorld () =
     [<Benchmark>]
     member _.FSharpIL() =
-        // TODO: Figure out how to share code with unit test.
         let mdle =
             { Name = Identifier.ofStr "HelloWorld.dll"
               Mvid = Guid.NewGuid() }
@@ -101,7 +103,7 @@ type HelloWorld () =
                                 |]
                                 |> ImmutableArray.Create<Opcode>
                               ImplFlags = MethodImplFlags.Zero
-                              MethodName = Identifier.ofStr "Method"
+                              MethodName = Identifier.ofStr "Main"
                               Flags =
                                 { Visibility = Public
                                   HideBySig = true }
@@ -129,6 +131,54 @@ type HelloWorld () =
             }
             |> ValidationResult.get
         PEFile.ofMetadata IsDll tables |> WritePE.toArray
+
+    [<Benchmark>]
+    member _.MonoCecil() =
+        use assembly =
+            AssemblyDefinition.CreateAssembly(
+                AssemblyNameDefinition("HelloWorld", Version()),
+                "HelloWorld.dll",
+                ModuleKind.Console
+            )
+
+        let mdle = assembly.MainModule
+
+        let mscorlib = AssemblyNameDefinition("System.Private.CoreLib", Version(5, 0, 0, 0))
+        mscorlib.PublicKeyToken <- [| 0x7cuy; 0xecuy; 0x85uy; 0xd7uy; 0xbeuy; 0xa7uy; 0x79uy; 0x8euy |]
+        mdle.AssemblyReferences.Add mscorlib
+
+        let consolelib = AssemblyNameDefinition("System.Console", Version(5, 0, 0, 0))
+        consolelib.PublicKeyToken <- [| 0xb0uy; 0x3fuy; 0x5fuy; 0x7fuy; 0x11uy; 0xd5uy; 0x0auy; 0x3auy |]
+        mdle.AssemblyReferences.Add consolelib
+
+        let console = TypeReference("System", "Console", null, consolelib) |> mdle.ImportReference
+        let tfmAttr = TypeReference("System.Runtime.Versioning", "TargetFrameworkAttribute", null, mscorlib) |> mdle.ImportReference
+
+        let str = ParameterDefinition mdle.TypeSystem.String
+        let writeLine = MethodReference("WriteLine", mdle.TypeSystem.Void, console) |> mdle.ImportReference
+        writeLine.Parameters.Add str
+        let tfmAttrCtor = MethodReference(".ctor", mdle.TypeSystem.Void, tfmAttr) |> mdle.ImportReference
+        tfmAttrCtor.Parameters.Add str
+
+        let program = TypeDefinition("HelloWorld", "Program", TypeAttributes.Public ||| TypeAttributes.Abstract ||| TypeAttributes.Sealed)
+        let main = MethodDefinition("Main", MethodAttributes.HideBySig ||| MethodAttributes.Public, mdle.TypeSystem.Void)
+
+        mdle.Types.Add program
+        program.BaseType <- mdle.TypeSystem.Object
+        program.Methods.Add main
+        ArrayType mdle.TypeSystem.String |> ParameterDefinition |> main.Parameters.Add
+        assembly.EntryPoint <- main
+
+        let body = main.Body.GetILProcessor()
+        body.Emit(OpCodes.Ldstr, "Hello World!")
+        body.Emit(OpCodes.Call, writeLine)
+        body.Emit(OpCodes.Ret)
+
+        let tfm = CustomAttribute tfmAttrCtor
+        CustomAttributeArgument(mdle.TypeSystem.String, ".NETCoreApp,Version=v5.0") |> tfm.ConstructorArguments.Add
+        assembly.CustomAttributes.Add tfm
+
+        assembly.Write(Stream.Null)
 
 [<EntryPoint>]
 let main argv =
