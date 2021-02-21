d83c040a058d5ad40704b0e210c46f01734a7250
davnavr <davidcalieln@gmail.com>
2021-02-21 12:48:12 -0800

    Update build.fsx to run tests on all tfms

--- build.fsx
+++ build.fsx
@@ -1,19 +1,13 @@
-#if FAKE_DEPENDENCIES
 #r "paket:
 nuget Fake.Core.Target
 nuget Fake.DotNet.Cli
-nuget Fake.IO.FileSystem
-//"
-#endif
-
+nuget Fake.IO.FileSystem //"
 #load "./.fake/build.fsx/intellisense.fsx"
 
 open Fake.Core
 open Fake.Core.TargetOperators
 open Fake.DotNet
 open Fake.IO.FileSystemOperators
-
-module DotNetCli = Fake.DotNet.DotNet
 
 let rootDir = __SOURCE_DIRECTORY__
 let testDir = rootDir </> "test"
@@ -26,32 +20,30 @@ let handleErr msg: ProcessResult -> _ =
         failwithf "Process exited with code %i: %s" ecode msg
     | _ -> ()
 
-let runProj args proj =
-    sprintf
-        "--project %s --no-restore --configuration Release -- %s"
-        proj
-        args
-    |> DotNetCli.exec id "run"
-
 Target.create "Clean" (fun _ ->
     slnFile
-    |> DotNetCli.exec id "clean"
+    |> DotNet.exec id "clean"
     |> ignore
 )
 
 Target.create "Build" (fun _ ->
-    DotNetCli.build
+    DotNet.build
         (fun opt ->
             { opt with
-                Configuration = DotNetCli.Release
+                Configuration = DotNet.Release
                 NoRestore = true })
         slnFile
 )
 
 Target.create "Test" (fun _ ->
-    testDir </> "FSharpIL.Tests" </> "FSharpIL.Tests.fsproj"
-    |> runProj ""
-    |> handleErr "One or more tests failed"
+    let proj = testDir </> "FSharpIL.Tests" </> "FSharpIL.Tests.fsproj"
+    for tfm in [ "netcoreapp3.1"; "net5.0" ] do
+        sprintf
+            "-p %s -c Release -f %s --no-build --no-restore"
+            proj
+            tfm
+        |> DotNet.exec id "run"
+        |> handleErr "One or more tests failed"
 )
 
 Target.create "Publish" (fun _ ->
