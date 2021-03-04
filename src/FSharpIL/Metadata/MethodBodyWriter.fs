8243453b944fa2cf66db41a94437d19387c8e28a
davnavr <davidcalieln@gmail.com>
2021-03-04 11:20:13 -0800

    Add opcodes to load the value of a field

--- src/FSharpIL/Metadata/MethodBodyWriter.fs
+++ src/FSharpIL/Metadata/MethodBodyWriter.fs
@@ -15,6 +15,8 @@ type internal MethodBodyContentImpl (writer, metadata, us: UserStringHeap) =
 type MethodBodyWriter internal (content: MethodBodyContentImpl) =
     new (content: MethodBodyContent) = MethodBodyWriter(content :?> MethodBodyContentImpl)
 
+    member private _.WriteMetadataToken(index, table) = MetadataToken.write index table content.Writer
+
     /// (0x00) Writes an instruction that does nothing (III.3.51).
     member _.Nop() = content.Writer.WriteU1 0uy
     /// (0x01) Writes an instruction used for debugging that "signals the CLI to inform the debugger that a breakpoint has been tripped" (III.3.16).
@@ -27,10 +29,10 @@ type MethodBodyWriter internal (content: MethodBodyContentImpl) =
     // TODO: Allow call to accept a MethodDef, MethodRef, or MethodSpec.
     /// <summary>(0x28) Writes an instruction that calls a method (III.3.19).</summary>
     /// <exception cref="T:FSharpIL.Metadata.IndexOwnerMismatchException"/>
-    member _.Call(SimpleIndex method: MemberRefIndex<MethodRef>) =
+    member this.Call(SimpleIndex method: MemberRefIndex<MethodRef>) =
         IndexOwner.checkIndex content.Metadata.Owner method
         content.Writer.WriteU1 0x28uy
-        MetadataToken.write (content.Metadata.MemberRef.IndexOf method) 0xAuy content.Writer
+        this.WriteMetadataToken(content.Metadata.MemberRef.IndexOf method, 0xAuy)
 
     /// <summary>
     /// (0x72) Writes an instruction that loads a string from the <c>#US</c> heap (III.4.16).
@@ -45,6 +47,22 @@ type MethodBodyWriter internal (content: MethodBodyContentImpl) =
         | _ ->
             content.Writer.WriteU1 0x72uy
             MetadataToken.userString str content.UserString content.Writer
+
+    // TODO: Allow a FieldRef to be used when loading an instance field.
+    /// <summary>
+    /// (0x7B) Writes an instruction that pushes the value of an object's field onto the stack (III.4.10).
+    /// </summary>
+    member this.Ldfld(field: SimpleIndex<FieldRow>) =
+        content.Writer.WriteU1 0x7Buy
+        this.WriteMetadataToken(content.Metadata.Field.IndexOf field, 0x4uy)
+
+    // TODO: Allow a FieldRef to be used when loading a static field.
+    /// <summary>
+    /// (0x7E) Writes an instruction that pushes the value of a static field onto the stack (III.4.14).
+    /// </summary>
+    member this.Ldsfld(field: SimpleIndex<FieldRow>) =
+        content.Writer.WriteU1 0x7Euy
+        this.WriteMetadataToken(content.Metadata.Field.IndexOf field, 0x4uy)
 
 [<AutoOpen>]
 module MethodBodyContentExtensions =
