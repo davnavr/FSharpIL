6abd212d6e8f66150032765502ade6b557e4667e
davnavr <davidcalieln@gmail.com>
2021-06-01 21:29:37 -0700

    Remove old CLI metadata code files

--- src/FSharpIL/Metadata/FieldRefModule.fs
+++
@@ -1,5 +0,0 @@
-﻿[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix); RequireQualifiedAccess>]
-module FSharpIL.Metadata.FieldRef
-
-let inline addRow (builder: CliMetadataBuilder) (field: FieldRef) =
-    builder.MemberRef.Add &field
