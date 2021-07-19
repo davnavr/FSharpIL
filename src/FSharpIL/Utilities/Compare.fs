a117346cec1b3400a7be7ee7e98fa2aea60c4694
davnavr <davidcalieln@gmail.com>
2021-07-19 00:15:14 -0700

    Update helper comparison functions to allow different type for second argument

--- src/FSharpIL/Utilities/Compare.fs
+++ src/FSharpIL/Utilities/Compare.fs
@@ -10,18 +10,35 @@ open System.Collections.Immutable
 /// </summary>
 [<RequireQualifiedAccess>]
 module Equatable =
-    let inline equals<'T when 'T :> IEquatable<'T>> (x: 'T) (y: 'T) = x.Equals(other = y)
+    let inline equals<'X, 'Y when 'X :> IEquatable<'Y>> (x: 'X) (y: 'Y) = x.Equals(other = y)
 
-    let lists (x: #IList<'T>) (y: #IList<'T>) =
+    let inline sequences x y =
+        let mutable xenumerator = (^Sequence : (member GetEnumerator: unit -> ^Enumerator) x)
+        let mutable yenumerator = (^Sequence : (member GetEnumerator: unit -> ^Enumerator) x)
+        let mutable eq, cont = true, true
+        while eq && cont do
+            let mutable xmoved = (^Enumerator : (member MoveNext: unit -> bool) xenumerator)
+            let mutable ymoved = (^Enumerator : (member MoveNext: unit -> bool) yenumerator)
+
+            cont <- xmoved && ymoved
+
+            if xmoved = ymoved then
+                if cont then
+                    eq <-
+                        equals (^Enumerator : (member Current: ^T) xenumerator) (^Enumerator : (member Current: ^T) yenumerator)
+            else eq <- false
+        eq
+
+    let lists (x: #IList<'X>) (y: #IList<'Y>) =
         let mutable eq, i = x.Count = y.Count, 0
         while eq && i < x.Count do
             eq <- equals x.[i] y.[i]
             i <- i + 1
         eq
 
-    let inline blocks (x: ImmutableArray<'T>) (y: ImmutableArray<'T>) = (x.IsDefaultOrEmpty && y.IsDefaultOrEmpty) || lists x y
+    let inline blocks (x: ImmutableArray<'X>) (y: ImmutableArray<'Y>) = (x.IsDefaultOrEmpty && y.IsDefaultOrEmpty) || lists x y
 
-    let inline voption (x: 'T voption) (y: 'T voption) =
+    let inline voption (x: 'X voption) (y: 'Y voption) =
         match x, y with
         | ValueSome x', ValueSome y' -> equals x' y'
         | ValueNone, ValueNone -> true
@@ -54,7 +71,7 @@ module Comparable =
     /// a value greater than zero if <paramref name="x"/> comes after <paramref name="y"/>,
     /// or zero if <paramref name="x"/> is equal to <paramref name="y"/>.
     /// </returns>
-    let inline comparison<'T when 'T :> IComparable<'T>> (x: 'T) (y: 'T) = x.CompareTo y
+    let inline comparison<'X, 'Y when 'X :> IComparable<'Y>> (x: 'X) (y: 'Y) = x.CompareTo y
 
 /// <summary>Less-than operator for objects implementing <see cref="T:System.IComparable`1"/>.</summary>
 let inline (<.) x y = Comparable.comparison x y < 0
